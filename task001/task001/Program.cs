3c296ee8d6b667dca25796b79e06a13822f18c4a
E.A.Koriachko@urfu.me <E.A.Koriachko@urfu.me>
2022-10-06 19:20:19 +0500

    task001 completed

---
+++ task001/task001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Цветаева М. И.");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Луч серебристый");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Эхо стонало, шумела река,");
+            Console.WriteLine("Ливень стучал тяжело,");
+            Console.WriteLine("Луч серебристый пронзил облака.");
+            Console.WriteLine("Им любовались мы долго, пока");
+            Console.WriteLine("Солнышко, солнце взошло!");
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+    }
+}
