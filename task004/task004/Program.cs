b021d0b109c3cb0275403208b80a01d2da6eae4e
E.A.Koriachko@urfu.me <E.A.Koriachko@urfu.me>
2022-10-14 23:36:50 +0500

    task 4 done

---
+++ task004/task004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            double y;
+            y = (double)Math.Abs((1+Math.Sqrt(x+1))/(1-Math.Sqrt(x-1)));
+
+            Console.WriteLine("y = " + y);
+
+            Console.ReadKey();
+        }
+    }
+}
