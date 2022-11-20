4b370b05df90b7cd609a19f56e983f3434aca9e3
E.A.Koriachko@urfu.me <E.A.Koriachko@urfu.me>
2022-11-20 22:20:48 +0500

    task 8 done

---
+++ task8/task8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            
+            if (Math.Sin(x) > 0)
+            {
+                Console.WriteLine(Math.Cos(x));
+            }
+            else if (Math.Sin(x) < 0)
+            {
+                Console.WriteLine(-Math.Cos(x));
+            }
+            else 
+            {
+                Console.WriteLine(x / Math.PI);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
